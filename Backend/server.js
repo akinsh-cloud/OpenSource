e152382468eb25615908c38806d29a02329586c0
akinsh-cloud <akankshasingh98765432@gmail.com>
2025-12-26 20:47:16 +0530

    Created mongodb connection and Added .env sample file

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,20 @@
 const app = require('./src/app');
-const dotenv = require('dotenv');
 
+//creating connection to database
+const connectDB = require('./src/db/db');
+connectDB()
+.then(() => { 
+    console.log('Database connected, starting server...');
+}).catch((err) => {
+    console.error('Failed to connect to the database:', err);
+    process.exit(1);
+});
+
+//loading environment variables
+const dotenv = require('dotenv');
 dotenv.config();
 
+//starting the server
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
